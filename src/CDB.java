302c544ae4df074562a3ca18d214a75133411ab0
Hugo Carvalho <hugonvsc@gmail.com>
2018-09-21 17:28:23 -0300

    Adicionando terceiro teste, triangulando e parametrizando

--- src/CDB.java
+++ src/CDB.java
@@ -59,4 +59,7 @@ public class CDB {
 
         return rendimento_bruto;
     }
+
+
+
 }
--- test/RendimentoBrutoTest.java
+++ test/RendimentoBrutoTest.java
@@ -1,29 +1,42 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class RendimentoBrutoTest {
-
-    @Test()
-    public void testCalculaRendimentoBruto1() {
-        //Criação do CDB com valores iniciais de dias, aplicação inicial, taxa de juros anual, Aliquota IR
-        CDB cdb = new CDB(60.0, 1000.0, 8.5, 22.5);
-
-
-        Double rendimentoBruto = cdb.calculaRendimentoBruto();
-
-        assertEquals(13.97, rendimentoBruto, 000.1);
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {60.0, 1000.0, 8.5, 22.5, 13.97},
+                {120.0, 500.0, 8.0, 22.5, 13.15},
+                {240.0, 3000.0, 9.0, 20.5, 177.53},
+        });
     }
 
+    @Parameter
+    public Double dias;
+    @Parameter(1)
+    public Double aplicacao_inicial;
+    @Parameter(2)
+    public Double juros_anual;
+    @Parameter(3)
+    public Double ir;
+    @Parameter(4)
+    public Double rendimento_bruto;
+
 
     @Test()
-    public void testCalculaRendimentoBruto2() {
-        //Criação do CDB com valores iniciais de dias, aplicação inicial, taxa de juros anual, Aliquota IR
-        CDB cdb = new CDB(120.0, 500.0, 8.0, 22.5);
+    public void testCalculaRendimentoBruto() {
+        CDB cdb = new CDB(dias, aplicacao_inicial, juros_anual,ir);
+        Double rendimento_bruto = cdb.calculaRendimentoBruto();
 
-        Double rendimentoBruto = cdb.calculaRendimentoBruto();
-
-        assertEquals(13.15, rendimentoBruto, 000.1);
+        assertEquals(this.rendimento_bruto, rendimento_bruto, 0.01);
     }
-
 }
