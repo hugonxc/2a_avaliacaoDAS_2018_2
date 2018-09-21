bfaef49a555221bfd7207c33e32083e893a2d940
Hugo Carvalho <hugonvsc@gmail.com>
2018-09-21 16:59:10 -0300

    Barra vermelha para novo teste para unidade anterior

--- test/RendimentoBrutoTest.java
+++ test/RendimentoBrutoTest.java
@@ -5,13 +5,24 @@ import static org.junit.Assert.assertEquals;
 public class RendimentoBrutoTest {
 
     @Test()
-    public void testCalculaRendimentoBruto() {
+    public void testCalculaRendimentoBruto1() {
         //Criação do CDB com valores iniciais de dias, aplicação inicial, taxa de juros anual, Aliquota IR
         CDB cdb = new CDB(60, 1000.0, 8.5, 22.5);
 
         Double rendimentoBruto = cdb.calculaRendimentoBruto();
 
         assertEquals(13.97, rendimentoBruto, 000.1);
+    }
+
+
+    @Test()
+    public void testCalculaRendimentoBruto2() {
+        //Criação do CDB com valores iniciais de dias, aplicação inicial, taxa de juros anual, Aliquota IR
+        CDB cdb = new CDB(120, 500.0, 8.0, 22.5);
+
+        Double rendimentoBruto = cdb.calculaRendimentoBruto();
+
+        assertEquals(13.15, rendimentoBruto, 000.1);
     }
 
 }
